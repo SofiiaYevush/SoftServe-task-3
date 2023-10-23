3988b43a19c72eb547f95798ebe2bbed22c56acc
Sofiia13 <sofiia.yevush@gmail.com>
2023-10-23 21:06:43 +0300

    Add html and js files

---
+++ task_3.js
@@ -0,0 +1,13 @@
+// ------------------- 1 --------------------
+
+function createArray(start, end)
+{
+  let i = start;
+  while (i <= end)
+  {
+    console.log(i);
+    ++i;
+  }
+}
+let arr = createArray(2, 9);
+console.log(arr);
